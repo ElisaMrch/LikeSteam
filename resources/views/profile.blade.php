e68ffe0a20dd645260dd47fe10c4ba4b3bf9ed2a
ElisaMrch <elisamarchand@orange.fr>
2021-11-15 11:48:00 +0100

    15/11

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -83,14 +83,12 @@
 </header>
 
 <main>
-
   <section class="py-5 text-center container">
     <div class="row py-lg-5">
       <div class="col-lg-6 col-md-8 mx-auto">
         <h1 class="fw-light">Profile</h1>
-        <p class="lead text-muted">Profile de NOMVARIABLEATROUVER </p>
+        <p class="lead text-muted">Profile de {{auth()->user()->name}}</p>
         <p>
-          <a href="#" class="btn btn-primary my-2">Modifier Mot de passe</a>
           <a href="#" class="btn btn-secondary my-2">Modifier Username</a>
         </p>
       </div>
